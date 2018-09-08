7c30e3335a41eb554d60be1f2893c9950ee70a61
thompson <thompson.wu@cardinfolink.com>
2018-09-08 11:23:18 +0800

    init project

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      home
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted(){},
+
+  methods: {}
+};
+</script>
+<style lang='scss' scoped>
+</style>
